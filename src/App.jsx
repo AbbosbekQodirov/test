af3b729db4096f90ecb6ce588b4d7cf53744a867
“Abbosbek” <abbosbekqodirov2501@gmail.com>
2023-08-25 11:34:31 +0500

    Farstroy loyihani h1 tegini o'zgartirdi

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>CODIAL</h1>
+      <h1>CODIAL Academy</h1>
       
     </>
   )
